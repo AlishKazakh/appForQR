ab9af0d6ea71e06cba4de438ef784566d4d1e3a8
Alisher Zhardemgaliyev <alisherzhardemgaliyev@gmail.com>
2023-03-05 01:39:01 +0100

    First commit

---
+++ app.js
@@ -0,0 +1,24 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(express.static("public"));
+
+
+
+app.get("/:dateQuantity", function(req, res){
+  res.render("post",{dateQuantity: req.params.dateQuantity})
+})
+
+
+
+
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Server started on port 3000");
+});
