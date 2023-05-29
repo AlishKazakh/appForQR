5f95348cd937de0685c7bdea20b173855587031d
Alisher Zhardemgaliyev <alisherzhardemgaliyev@gmail.com>
2023-05-29 21:03:37 +0200

    allign center and header added

--- app.js
+++ app.js
@@ -19,6 +19,6 @@ app.get("/:dateQuantity", function(req, res){
 
 
 
-app.listen(process.env.PORT || 3000, function() {
-  console.log("Server started on port 3000");
+app.listen(process.env.PORT || 5000, function() {
+  console.log("Server started on port 5000");
 });
